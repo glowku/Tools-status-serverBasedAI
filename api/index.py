a9209c94218a5963ee9336a99e1d661d36490193
GGE Official <guillaumegamaury@gmail.com>
2025-09-21 19:03:13 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,9 @@
-# api/index.py
 import sys
 import os
 
-# Ajouter le répertoire parent au chemin
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 
-# Handler pour Vercel
 def handler(event, context):
     return app(event, context)
